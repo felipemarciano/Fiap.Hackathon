440a09737ea625be63777ce86593e264470d7cc5
Guilherme Augusto <guilhermeaugusto04@hotmail.com.br>
2024-03-16 15:33:52 -0300

    remove version

--- Fiap.Hackaton.API/Controllers/UploadController.cs
+++ Fiap.Hackaton.API/Controllers/UploadController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Fiap.Hackaton.API.Controllers
 {
     [ApiController]
-    [ApiVersion("1.0")]
     [Route("[controller]")]
     public class UploadController : ControllerBase
     {
@@ -20,15 +19,15 @@ namespace Fiap.Hackaton.API.Controllers
         }
 
         [AllowAnonymous]
-        [HttpGet("/v{version:apiVersion}/upload:start", Name = "upload-process")]
+        [HttpGet("/v1/upload:start", Name = "upload-process")]
         public async Task CreateProcessRequest(UploadProcessRequest request)
         {
-            await _requestService.CreateRequestProcessing(request.Name);
+            await _requestService.CreateRequestProcessing(request.Base64Video);
         }
 
 
         [AllowAnonymous]
-        [HttpGet("/v{version:apiVersion}/uploads", Name = "list-uploads-history")]
+        [HttpGet("/v1/uploads", Name = "list-uploads-history")]
         public async Task<IActionResult> GetUploadsHistories()
         {
             var result = await _requestService.GetAllUploadsAsync();
